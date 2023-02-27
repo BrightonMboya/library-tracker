364c4a73f33888c6c2f39987b41f5f35775f9de1
BrightonMboya <b.mboya@alustudent.com>
2023-02-27 15:02:28 +0200

    added the nav on auth pages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,12 @@
 // "use-client";
-
 import Input from "../components/UI/Input";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import type { NextPage } from "next";
 import type { ChangeEvent } from "react";
-import { inferProcedureInput } from "@trpc/server";
-import { AppRouter } from "../server/api/root";
 import { signIn } from "next-auth/react";
+import { Nav } from "../components/LandingPage";
 
 const Login: NextPage = () => {
   const initialFormStates = {
@@ -19,77 +17,80 @@ const Login: NextPage = () => {
   const [formData, setFormData] = useState(initialFormStates);
 
   return (
-    <div className="mt-5 flex min-h-screen flex-col items-center justify-center">
-      <div>
-        <h3 className="text-center text-[2rem] font-medium tracking-wide">
-          Login
-        </h3>
-        <button className="mt-3 flex w-[250px] cursor-pointer items-center gap-5 rounded-md border-[1px]  border-[rgba(142,142,147,0.22)] px-2 py-2 text-[#303030]">
-          <span>
-            <FcGoogle size={25} />
-          </span>
-          Continue with Google
-        </button>
-      </div>
-      <form
-        className="mt-5 space-y-5"
-        onSubmit={async (e) => {
-          e.preventDefault();
-          const loginData = {
-            email: formData.email,
-            password: formData.password,
-          };
-
-          try {
-            await signIn("credentials", {
-              ...loginData,
-              callbackUrl: "/libraries",
-            });
-          } catch (cause) {
-            console.log(cause, "caused the error");
-          }
-        }}
-      >
-        <Input
-          type="email"
-          label="Email"
-          placeholder="Segun@yahoo.com"
-          value={formData.email}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            setFormData({
-              ...formData,
-              email: e.target.value,
-            });
-          }}
-        />
-        <Input
-          type="password"
-          label="Password"
-          placeholder="*********"
-          value={formData.password}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            setFormData({
-              ...formData,
-              password: e.target.value,
-            });
-          }}
-        />
-
-        <button
-          type="submit"
-          className="mt-5 w-[250px] cursor-pointer rounded-md bg-blue px-2 py-2 font-medium text-white"
-        >
-          Continue With Email
-        </button>
-
-        <div className="flex space-x-1">
-          <p>I don&apos;t have an account, </p>
-          <Link href="/signup">
-            <p className="cursor-pointer font-medium text-blue">sign Up</p>
-          </Link>
+    <main>
+      <Nav />
+      <div className="mt-5 flex min-h-screen flex-col items-center justify-center">
+        <div>
+          <h3 className="text-center text-[2rem] font-medium tracking-wide">
+            Login
+          </h3>
+          <button className="mt-3 flex w-[250px] cursor-pointer items-center gap-5 rounded-md border-[1px]  border-[rgba(142,142,147,0.22)] px-2 py-2 text-[#303030]">
+            <span>
+              <FcGoogle size={25} />
+            </span>
+            Continue with Google
+          </button>
         </div>
-      </form>
-    </div>
+        <form
+          className="mt-5 space-y-5"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const loginData = {
+              email: formData.email,
+              password: formData.password,
+            };
+
+            try {
+              await signIn("credentials", {
+                ...loginData,
+                callbackUrl: "/libraries",
+              });
+            } catch (cause) {
+              console.log(cause, "caused the error");
+            }
+          }}
+        >
+          <Input
+            type="email"
+            label="Email"
+            placeholder="Segun@yahoo.com"
+            value={formData.email}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setFormData({
+                ...formData,
+                email: e.target.value,
+              });
+            }}
+          />
+          <Input
+            type="password"
+            label="Password"
+            placeholder="*********"
+            value={formData.password}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setFormData({
+                ...formData,
+                password: e.target.value,
+              });
+            }}
+          />
+
+          <button
+            type="submit"
+            className="mt-5 w-[250px] cursor-pointer rounded-md bg-blue px-2 py-2 font-medium text-white"
+          >
+            Continue With Email
+          </button>
+
+          <div className="flex space-x-1">
+            <p>I don&apos;t have an account, </p>
+            <Link href="/signup">
+              <p className="cursor-pointer font-medium text-blue">sign Up</p>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 };
 
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -9,6 +9,8 @@ import type { inferProcedureInput } from "@trpc/server";
 import type { AppRouter } from "../server/api/root";
 import { useState, ChangeEvent } from "react";
 import { signIn } from "next-auth/react";
+import { Nav } from "../components/LandingPage";
+
 const Signup: NextPage = () => {
   const router = useRouter();
 
@@ -26,94 +28,97 @@ const Signup: NextPage = () => {
   const [formData, setFormData] = useState(formStates);
 
   return (
-    <div className="mt-5 flex min-h-screen flex-col items-center justify-center">
-      <div>
-        <h3 className="text-center text-[2rem] font-medium tracking-wide">
-          Sign Up
-        </h3>
-        <button
-          className="mt-3 flex w-[250px] cursor-pointer items-center gap-5 rounded-md border-[1px]  border-[rgba(142,142,147,0.22)] px-2 py-2 text-[#303030]"
-          onClick={() => signIn("google", { callbackUrl: "/libraries" })}
-        >
-          <span>
-            <FcGoogle size={25} />
-          </span>
-          Continue with Google
-        </button>
-      </div>
-      <form
-        className="mt-5 space-y-5"
-        onSubmit={async (e) => {
-          e.preventDefault();
-          type Input = inferProcedureInput<AppRouter["user"]["signup"]>;
-          const input: Input = {
-            fullName: formData.fullName,
-            email: formData.email,
-            password: formData.password,
-          };
-
-          try {
-            await userRouter.mutateAsync(input);
-            setFormData(formStates);
-            router.push("/login");
-          } catch (cause) {
-            console.error({ cause }, "Failed to create the user!!");
-          }
-        }}
-      >
-        <Input
-          label="Full Name"
-          placeholder="Segun Favour"
-          type="text"
-          value={formData.fullName}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            setFormData({
-              ...formData,
-              fullName: e.target.value,
-            });
-          }}
-        />
-        <Input
-          label="Email"
-          placeholder="Segun@yahoo.com"
-          type="email"
-          value={formData.email}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            setFormData({
-              ...formData,
-              email: e.target.value,
-            });
-          }}
-        />
-
-        <Input
-          label="Password"
-          placeholder="*****"
-          type="password"
-          value={formData.password}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            setFormData({
-              ...formData,
-              password: e.target.value,
-            });
-          }}
-        />
-
-        <button
-          type="submit"
-          className="mt-5 w-[250px] cursor-pointer rounded-md bg-blue px-2 py-2 font-medium text-white"
-        >
-          Continue With Email
-        </button>
-
-        <div className="flex space-x-1">
-          <p>I already have an account, </p>
-          <Link href="/login">
-            <p className="cursor-pointer font-medium text-blue">login</p>
-          </Link>
+    <main>
+      <Nav />
+      <div className="mt-5 flex min-h-screen flex-col items-center justify-center">
+        <div>
+          <h3 className="text-center text-[2rem] font-medium tracking-wide">
+            Sign Up
+          </h3>
+          <button
+            className="mt-3 flex w-[250px] cursor-pointer items-center gap-5 rounded-md border-[1px]  border-[rgba(142,142,147,0.22)] px-2 py-2 text-[#303030]"
+            onClick={() => signIn("google", { callbackUrl: "/libraries" })}
+          >
+            <span>
+              <FcGoogle size={25} />
+            </span>
+            Continue with Google
+          </button>
         </div>
-      </form>
-    </div>
+        <form
+          className="mt-5 space-y-5"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            type Input = inferProcedureInput<AppRouter["user"]["signup"]>;
+            const input: Input = {
+              fullName: formData.fullName,
+              email: formData.email,
+              password: formData.password,
+            };
+
+            try {
+              await userRouter.mutateAsync(input);
+              setFormData(formStates);
+              router.push("/login");
+            } catch (cause) {
+              console.error({ cause }, "Failed to create the user!!");
+            }
+          }}
+        >
+          <Input
+            label="Full Name"
+            placeholder="Segun Favour"
+            type="text"
+            value={formData.fullName}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setFormData({
+                ...formData,
+                fullName: e.target.value,
+              });
+            }}
+          />
+          <Input
+            label="Email"
+            placeholder="Segun@yahoo.com"
+            type="email"
+            value={formData.email}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setFormData({
+                ...formData,
+                email: e.target.value,
+              });
+            }}
+          />
+
+          <Input
+            label="Password"
+            placeholder="*****"
+            type="password"
+            value={formData.password}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setFormData({
+                ...formData,
+                password: e.target.value,
+              });
+            }}
+          />
+
+          <button
+            type="submit"
+            className="mt-5 w-[250px] cursor-pointer rounded-md bg-blue px-2 py-2 font-medium text-white"
+          >
+            Continue With Email
+          </button>
+
+          <div className="flex space-x-1">
+            <p>I already have an account, </p>
+            <Link href="/login">
+              <p className="cursor-pointer font-medium text-blue">login</p>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 };
 
